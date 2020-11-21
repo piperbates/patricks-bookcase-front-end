749cdb448f225f14ec8cb5bc3af8a04ca02fbc3b
Piper <bates.sarah42@gmail.com>
2020-11-21 23:43:59 +0000

    finishing touches to mlp

--- js/generic.js
+++ js/generic.js
@@ -2,3 +2,14 @@ function changeFormColour(input){
     input.classList.remove("form-required")
     input.classList.add("form-required")
 }
+let nav = document.querySelector("nav");
+
+//toggle menu
+function toggleMenu(){
+    nav.classList.toggle("hidden")
+
+}
+
+let toggle = document.getElementById("toggle");
+
+toggle.addEventListener("click", toggleMenu);
